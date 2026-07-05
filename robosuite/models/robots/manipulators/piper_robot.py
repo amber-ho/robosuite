c3924aad16cd9c9e74e7420c1f40f16a9b73b4db
amber-ho <amba101065@gmail.com>
2026-07-05 23:30:52 +0800

    Minor changes of robot position

--- robosuite/models/robots/manipulators/piper_robot.py
+++ robosuite/models/robots/manipulators/piper_robot.py
@@ -36,7 +36,7 @@ class Piper(ManipulatorModel):
         return {
             "bins": (-0.5, -0.1, 0),
             "empty": (-0.6, 0, 0),
-            "table": lambda table_length: (-0.16 - table_length / 2, 0, 0),
+            "table": lambda table_length: (- table_length / 2, -0.1, 0),
         }
 
     @property
